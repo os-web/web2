b8652c0a875d2b675f5c776fdf47270bf7c814a8
os-web <ivanshcherban7@gmail.com>
2020-06-26 18:21:29 +0300

    Signed-off-by: os-web <ivanshcherban7@gmail.com>

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Content() {
+    return <div className="content">
+    <div>
+      <img src="https://www.ivetta.ua/wp-content/uploads/2010/08/Sauf-Bich-South-Beach.jpg"></img>
+    </div>
+    <div>
+      ava+discription 
+    </div>
+    <div>
+      My posts
+      <div>
+      New post 
+      </div>
+      <div>
+        post 1
+      </div> 
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+  ;
+}
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+    return <header className="header">
+    <img src="https://s2.logaster.com/static/v3/img/products/logo.png" className="App-logo" alt="logo" /> 
+  </header>
+  ;
+}
+
+export default Header;
